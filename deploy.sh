ad5731870988ccbdbbf1e7bd419d93cb18f8d7bf
Matt Hoyle <matthoyle@fastmail.com>
2019-12-07 10:44:52 -0500

    Add AWS keys and set kubeconfig

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+printf "%s\n%s\nus-east-1\njson" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" | aws configure --profile default
+aws eks --region us-east-1 update-kubeconfig --name multi-cluster
+echo "$DOCKERHUB_PASS" | docker login --username primarydeploy --password-stdin
 docker build -t matthoyle/multi-client:latest -t matthoyle/multi-client:$GITHUB_SHA -f ./client/Dockerfile ./client
 docker build -t matthoyle/multi-server:latest -t matthoyle/multi-server:$GITHUB_SHA -f ./server/Dockerfile ./server
 docker build -t matthoyle/multi-worker:latest -t matthoyle/multi-worker:$GITHUB_SHA -f ./worker/Dockerfile ./worker
-echo "$DOCKERHUB_PASS" | docker login --username primarydeploy --password-stdin
 docker push matthoyle/multi-client:latest
 docker push matthoyle/multi-server:latest
 docker push matthoyle/multi-worker:latest
