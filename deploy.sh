2f6eca6e1dfdef7c7f5638135108981d90ee26e4
Matt Hoyle <matthoyle@fastmail.com>
2019-12-07 11:03:03 -0500

    Setup logins to eks and docker

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-printf "%s\n%s\nus-east-1\njson" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" | aws configure --profile default
-aws eks --region us-east-1 update-kubeconfig --name multi-cluster
-echo "$DOCKERHUB_PASS" | docker login --username primarydeploy --password-stdin
 docker build -t matthoyle/multi-client:latest -t matthoyle/multi-client:$GITHUB_SHA -f ./client/Dockerfile ./client
 docker build -t matthoyle/multi-server:latest -t matthoyle/multi-server:$GITHUB_SHA -f ./server/Dockerfile ./server
 docker build -t matthoyle/multi-worker:latest -t matthoyle/multi-worker:$GITHUB_SHA -f ./worker/Dockerfile ./worker
