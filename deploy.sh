a2f57f146eca7e2111ea690de79121790010ea00
Matt Hoyle <matthoyle@fastmail.com>
2019-12-07 10:33:22 -0500

    Add dockerhub pass env var

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t matthoyle/multi-client:latest -t matthoyle/multi-client:$GITHUB_SHA -f ./client/Dockerfile ./client
 docker build -t matthoyle/multi-server:latest -t matthoyle/multi-server:$GITHUB_SHA -f ./server/Dockerfile ./server
 docker build -t matthoyle/multi-worker:latest -t matthoyle/multi-worker:$GITHUB_SHA -f ./worker/Dockerfile ./worker
+echo "$DOCKERHUB_PASS" | docker login --username primarydeploy --password-stdin
 docker push matthoyle/multi-client:latest
 docker push matthoyle/multi-server:latest
 docker push matthoyle/multi-worker:latest
